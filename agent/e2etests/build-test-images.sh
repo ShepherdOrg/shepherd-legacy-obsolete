e519b5d428aba43ee8e9d3919c94dece9be7c023
Guðlaugur S. Egilsson <gulli@kolibri.is>
2019-10-09 14:40:02 +0000

    Adding test docker build using json metadata format

--- agent/e2etests/build-test-images.sh
+++ agent/e2etests/build-test-images.sh
@@ -1,19 +1,11 @@
 #!/usr/bin/env bash
 THISDIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
 
+set -eao pipefail
+
 echo "Rebuilding test images"
-pushd .
-cd ${THISDIR}/test-infrastructure-image/ && ./build-docker.sh
-popd
-pushd .
-cd ${THISDIR}/test-migration-image/ && ./build-docker.sh
-popd
-pushd .
-cd ${THISDIR}/test-image && ./build-docker.sh
-popd
-pushd .
-cd ${THISDIR}/test-image2 && ./build-docker.sh
-popd
-pushd .
-cd ${THISDIR}/test-image3 && ./build-docker.sh
-popd
+(cd ${THISDIR}/test-infrastructure-image/ && ./build-docker.sh)
+(cd ${THISDIR}/test-migration-image/ && ./build-docker.sh)
+(cd ${THISDIR}/test-image && ./build-docker.sh)
+(cd ${THISDIR}/test-image2 && ./build-docker.sh)
+(cd ${THISDIR}/test-image3 && ./build-docker.sh)
